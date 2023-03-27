e6c2ccfd944b32f1e3c11343634b11088ec5b32f
EmilyWynn <79488196+EmilyWynn@users.noreply.github.com>
2023-03-27 16:40:47 -0500

    Update EDGAR-protein-grabber.py

--- EDGAR-protein-grabber.py
+++ EDGAR-protein-grabber.py
@@ -3,13 +3,16 @@ import csv, os
 li = os.listdir(os.getcwd())
 inds = filter(lambda x: ".csv" in x, li)
 
+#set the path to the gff files, edit below to point to the folder with your GFF files
+GFFpath = "/path/to/gff/files/"
+
 for ind in inds:
     with open(ind.replace(".csv","AA.fasta"), "w") as outfile:
         with open(ind) as f:
             reader = csv.reader(f)
             for row in reader:
                 gff_filename = row[0] + '.gff'
-                with open(gff_filename) as gff_file:
+                with open(GFFpath + gff_filename) as gff_file:
                     for cell in row[1:]:
                         if cell and cell != '-':
                             for line in gff_file:
